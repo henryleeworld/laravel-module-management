e488385d034509b90939148edea44d8e4e55ca89
Henry Lee <henryleeworld@gmail.com>
2024-07-01 00:16:56 +0800

    feat:更新最新資料

--- modules/ContactManagement/Resources/views/admin/contactCompanies/show.blade.php
+++ modules/ContactManagement/Resources/views/admin/contactCompanies/show.blade.php
@@ -9,7 +9,7 @@
     <div class="card-body">
         <div class="form-group">
             <div class="form-group">
-                <a class="btn btn-default" href="{{ route('admin.contact-companies.index') }}">
+                <a class="btn btn-light" href="{{ route('admin.contact-companies.index') }}">
                     {{ trans('global.back_to_list') }}
                 </a>
             </div>
@@ -58,7 +58,7 @@
                 </tbody>
             </table>
             <div class="form-group">
-                <a class="btn btn-default" href="{{ route('admin.contact-companies.index') }}">
+                <a class="btn btn-light" href="{{ route('admin.contact-companies.index') }}">
                     {{ trans('global.back_to_list') }}
                 </a>
             </div>
